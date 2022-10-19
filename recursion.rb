9d96bad079796dfda4a23f8c1cb9e107cba53e1a
Rex Kho <rexkho510@gmail.com>
2022-10-18 23:16:26 -0700

    <finished recursion homework.

---
+++ recursion.rb
@@ -0,0 +1,70 @@
+def sum_to(n)
+    return 1 if n == 1
+    return nil if n < 0
+    n + sum_to(n-1)
+end
+
+    #  sum_to(5)  # => returns 15
+    #  sum_to(1)  # => returns 1
+    #  sum_to(9)  # => returns 45
+    # sum_to(-8)  # => returns nil
+
+def add_numbers(nums_array)
+    return [] if nums_array[0] == nil
+    return nums_array[0] if nums_array.length == 1
+
+    nums_array[0] + add_numbers(nums_array[1..-1])
+end
+
+# p add_numbers([1,2,3,4]) # => returns 10
+# p add_numbers([3]) # => returns 3
+# p add_numbers([-80,34,7]) # => returns -39
+# p add_numbers([]) # => returns nil
+
+def gamma_fnc(n)
+    return nil if n == 0
+    return 1 if n == 1
+    return 1 if n == 2
+    (n-1) * gamma_fnc(n-1)
+
+end
+
+# p gamma_fnc(0)  # => returns nil
+# p gamma_fnc(1)  # => returns 1
+# p gamma_fnc(4)  # => returns 6
+# p gamma_fnc(8)  # => returns 5040
+
+def ice_cream_shop(flavors, favorite)
+    if flavors[0] == nil
+        return false
+    end
+    if flavors[0] == favorite
+        return true
+    end
+    ice_cream_shop(flavors[1..-1], favorite)
+end
+
+
+
+# p ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  # => returns false
+# p ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  # => returns true
+# p ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  # => returns false
+# p ice_cream_shop(['moose tracks'], 'moose tracks')  # => returns true
+# p ice_cream_shop([], 'honey lavender')  # => returns false
+
+def reverse(str)
+    return "" if str == ""
+    if str.length == 1
+        return str
+    end
+
+    str[-1] + reverse(str[0...-1])
+
+end
+
+p reverse("house") # => "esuoh"
+p reverse("dog") # => "god"
+p reverse("atom") # => "mota"
+p reverse("q") # => "q"
+p reverse("id") # => "di"
+p reverse("") # => ""
