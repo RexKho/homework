3ae0b808c9d4d46edaf5e7b8daf587d13e079266
Rex Kho <rexkho510@gmail.com>
2022-10-27 23:16:18 -0700

    Tried Big O octopus demo, stuck on log issue

---
+++ w4d5/octo.rb
@@ -0,0 +1,20 @@
+arr = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+
+# longest = arr[0]
+# arr.each do |ele|
+#     if arr[0].length < ele.length
+#         longest = ele   
+#     end
+# end
+# puts longest
+
+def merge_sort(arr, &prc)
+    prc ||= 
+    mid = arr.length /2
+    return arr[mid] if arr[mid ==]
+    left = arr[0...mid]
+    right = arr[mid..-1]
+
+end
+
+
