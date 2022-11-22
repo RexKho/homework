0fc9eefcefde798e9cba18e728bb0719480d8ac9
Rex Kho <rexkho510@gmail.com>
2022-11-21 23:43:23 -0800

    Started Javascript Homework

---
+++ w8d1/random.js
@@ -0,0 +1,74 @@
+// function mysteryScoping1() {
+//     var x = 'out of block';
+//     if (true) {
+//       var x = 'in block';  
+//       console.log(x);
+//     }
+//     console.log(x);
+//   }
+
+// function mysteryScoping2() {
+//     const x = 'out of block';
+//     if (true) {
+//       const x = 'in block';  
+//       console.log(x);
+//     }
+//     console.log(x);
+//   }
+  
+// function mysteryScoping3() {
+//     const x = 'out of block';
+//     if (true) {
+//       var x = 'in block';  
+//       console.log(x);
+//     }
+//     console.log(x);
+//   }
+
+// function mysteryScoping4() {
+//     let x = 'out of block';
+//     if (true) {
+//       let x = 'in block';  
+//       console.log(x);
+//     }
+//     console.log(x);
+//   }
+
+
+// function mysteryScoping5() {
+//     let x = 'out of block';
+//     if (true) {
+//       let x = 'in block';  
+//       console.log(x);
+//     }
+//     let x = 'out of block again';
+//     console.log(x);
+//   }
+
+
+function madLib(arg1, arg2, arg3) {
+    // let var1 = arg1.toUpperCase;
+
+    console.log(`We shall ${arg1.toUpperCase()} the ${arg2.toUpperCase()} ${arg3.toUpperCase()}`);
+}
+
+// madLib('make', 'best', 'guac');
+
+function isSubstring( searchString, subString) {
+    console.log(searchString.includes(subString))
+
+}
+// isSubstring("time to program", "time")
+
+
+// isSubstring("Jump for joy", "joys")
+
+
+function fizzBuzz(array) {
+    const newarr = [];
+    array.forEach(el => {
+
+
+    })
+
+}
